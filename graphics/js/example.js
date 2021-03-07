1280889d571c9ccc9021dbfff4f14630b74f4124
BabtainM <80225750+BabtainM@users.noreply.github.com>
2021-03-07 17:36:10 -0500

    Update example.js

--- graphics/js/example.js
+++ graphics/js/example.js
@@ -1,70 +1,127 @@
-var camera, scene, renderer;
-var cameraControls;
-var clock = new THREE.Clock();
+
+let camera, scene, renderer;
+let cameraControls;
+let clock = new THREE.Clock();
+let currentMesh, currentMat, geom;
+let radius = 3;
+let ring = new THREE.Object3D();
+
 
 function createScene() {
-    var triangle = makeTriangle();
-    var axes = new THREE.AxesHelper(10);
-    scene.add(triangle);
-    scene.add(axes);
+    currentMat = new THREE.MeshLambertMaterial({color: "yellow", side: THREE.DoubleSide}); 
+    let light = new THREE.PointLight(0xFFFFFF, 1.0, 1000 );
+    light.position.set(0, 0, 40);
+    let light2 = new THREE.PointLight(0xFFFFFF, 1.0, 1000 );
+    light2.position.set(0, 0, -40);
+    let ambientLight = new THREE.AmbientLight(0x333333);
+    scene.add(light);
+    scene.add(light2);
+    scene.add(ambientLight);
+    scene.add(ring);
+    updateRing();
 }
 
-function makeTriangle() {
-    var geom = new THREE.Geometry();
-    var a = new THREE.Vector3(0, 0, 0);
-    var b = new THREE.Vector3(4, 0, 0);
-    var c = new THREE.Vector3(0, 8, 0);
-    geom.vertices.push(a, b, c, a)
-    var red = new THREE.Color(1, 0, 0);
-    var green = new THREE.Color(0, 1, 0);
-    var blue = new THREE.Color(0, 0, 1);
-    geom.colors.push(red, green, blue, red);
-    var args = {vertexColors: true, linewidth: 4};
-    var mat = new THREE.LineBasicMaterial(args);
-    var line = new THREE.Line(geom, mat, THREE.LineStrip);
-    return line;
+function createSolidRing(r = 10, m = Math.PI / 4, nbrSolids = 50) {
+    mat = new THREE.MeshLambertMaterial({transparent: true})
+    let h = 2 * m / (20 - 1);
+    let points = [];
+    for (let i = 0; i < 20; i++) {
+        let a = -m + i * h;  
+        let x = r * Math.cos(a);
+        let y = r * Math.sin(a);
+        let g = new THREE.Vector2(x, y);
+        points.push(g);
+    }
+    return new THREE.LatheGeometry(points, nbrSolids);
+}
+
+var controls = new function() {
+    this.nbrSolids = 50;
+    this.scale = 0.2;
+    this.opacity = 0.5;
+}
+
+function initGui() {
+    let gui = new dat.GUI();
+    gui.add(controls, 'nbrSolids', 3, 50).step(0.1).onChange(updateRing);
+    gui.add(controls, 'opacity', 0.0, 1.0).step(0.1).onChange(updateOpacityScale);
+    gui.add(controls, 'scale', 0.1, 0.7).step(0.1).onChange(updateRing);
+}
+
+function updateRing() {
+    if (currentMesh) {
+        scene.remove(currentMesh);
+       updateOpacityScale();
+    }
+
+    geom = createSolidRing(radius, controls.scale, controls.nbrSolids, controls.opacity);
+    if (geom) {
+        currentMesh = new THREE.Object3D;
+        currentMesh.add(new THREE.Mesh(geom, currentMat));
+        scene.add(currentMesh);
+    }
+}
+
+function updateOpacityScale() {
+    if (currentMesh) {
+        let scale = controls.scale;
+        let opacity = controls.opacity; 
+
+        for (let c of ring.children) {
+            let mesh = c.children[0];
+            mesh.material.opacity = opacity;
+            currentMesh.scale.set(scale, scale, scale);
+        }
+    }
 }
 
 function animate() {
-	window.requestAnimationFrame(animate);
-	render();
+    window.requestAnimationFrame(animate);
+    render();
 }
 
 function render() {
     var delta = clock.getDelta();
     cameraControls.update(delta);
-	renderer.render(scene, camera);
+    currentMesh.color = new THREE.Color(controls.color);
+    currentMesh.opacity = controls.opacity;
+    renderer.render(scene, camera);
 }
 
 function init() {
-	var canvasWidth = window.innerWidth;
-	var canvasHeight = window.innerHeight;
-	var canvasRatio = canvasWidth / canvasHeight;
-	scene = new THREE.Scene();
+    var canvasWidth = window.innerWidth;
+    var canvasHeight = window.innerHeight;
+    var canvasRatio = canvasWidth / canvasHeight;
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({antialias : true});
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.setSize(canvasWidth, canvasHeight);
+    renderer.setClearColor(0x000000, 1.0);
+    renderer.setAnimationLoop( () => {
+    renderer.render(scene, camera);
+    });
 
-	renderer = new THREE.WebGLRenderer({antialias : true, preserveDrawingBuffer: true});
-	renderer.gammaInput = true;
-	renderer.gammaOutput = true;
-	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColor(0x000000, 1.0);
-	camera = new THREE.PerspectiveCamera( 40, canvasRatio, 1, 1000);
-	camera.position.set(0, 0, 40);
-	camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+    camera = new THREE.PerspectiveCamera( 40, canvasRatio, 1, 1000);
+    camera.position.set(0, 10, 14);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 }
 
 function addToDOM() {
-	var container = document.getElementById('container');
-	var canvas = container.getElementsByTagName('canvas');
-	if (canvas.length>0) {
-		container.removeChild(canvas[0]);
-	}
-	container.appendChild( renderer.domElement );
+    var container = document.getElementById('container');
+    var canvas = container.getElementsByTagName('canvas');
+    if (canvas.length>0) {
+        container.removeChild(canvas[0]);
+    }
+    container.appendChild( renderer.domElement );
 }
 
 init();
 createScene();
+initGui();
 addToDOM();
-render();
 animate();
+render();
+
+
